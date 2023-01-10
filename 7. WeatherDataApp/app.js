ed14be3e5760946453cf7629e682cfafeb5fc49e
claudioferreira <claudioferreirajr@hotmail.com>
2023-01-10 18:30:00 -0300

    app weather finished

--- 7. WeatherDataApp/app.js
+++ 7. WeatherDataApp/app.js
@@ -1,28 +1,63 @@
+// Variaveis e selecao de elementos
+const apikey = "";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const humidityElement = document.querySelector('#humidity span');
+const windElement = document.querySelector('#wind span');
+
+//container que iremos mudar a classe para aparecer na tela
+const weatherContainer = document.querySelector('#weather-data');
 
 
-const h1 = document.querySelector('h1');
-const inputVal = document.querySelector('.val');
-const btn = document.querySelector('.btn');
-const url1 = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={APIKEY}}';
 
+// Funçoes
+//funcao assincrona para obter os dados da API
+const getWeatherData = async(city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
 
-//inputVal.value = 'test';
-//btn.textContent = 'Click Me';
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
 
-btn.addEventListener('click', (e) => {
-  console.log('ready');
-  getValues(url1);
-})
-
-function getValues(url) {
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch( (err) => {
-    console.log(err);
-  })
-
-
+  return data;
 }
+//funcao para motrar na tela os dados extraidos
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  //mostra o quadro com a informação apos a pesquisa
+  weatherContainer.classList.remove("hide");
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+//evento que capta a tecla 'enter' para desencadear o evento
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
