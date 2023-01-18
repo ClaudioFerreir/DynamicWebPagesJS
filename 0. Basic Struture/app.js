557c62e381b53675792ea003487c4d8be65d2cf3
claudioferreira <claudioferreirajr@hotmail.com>
2023-01-18 18:10:49 -0300

    inittial config

--- 0. Basic Struture/app.js
+++ 0. Basic Struture/app.js
@@ -1,8 +1,12 @@
-const btn = document.querySelector('.btn');
+const btn1 = document.querySelector('.btn');
 const h1 = document.querySelector('.h1');
 const output = document.querySelector('.output');
 const inputVal = document.querySelector('.val');
 
-btn.addEventListener('click', (e) => {
+window.addEventListener('DOMContentLoaded', (e) => {
+  console.log('DOM ready'); // msg sempre que o conteudo do DOM é carregado
+})
+
+btn1.addEventListener('click', (e) => {
   console.log('ready');
 })
---
+++ 10. OpenTrivia DB /app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+const h1 = document.querySelector('.h1');
+const output = document.querySelector('.output');
+const inputVal = document.querySelector('.val');
+
+btn.addEventListener('click', (e) => {
+  console.log('ready');
+})
