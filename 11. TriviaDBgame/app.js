a48c9f9f80bbf6246dd60534fc8faa13ad7b53f6
claudioferreira <claudioferreirajr@hotmail.com>
2023-02-24 10:39:25 -0300

    anwers options

--- 11. TriviaDBgame/app.js
+++ 11. TriviaDBgame/app.js
@@ -30,20 +30,33 @@ function popPage (url) {
     })
 }
 
-function outputPage () {
+function outputPage() {
+  output.innerHTML = '';
   let question = game.que[game.question]; // pergunta e indice
   game.question++; // move to next question
   console.log(question);
   let answers = question.incorrect_answers;
   answers.push(question.correct_answer);
   console.log(answers);
-
+  const mainDiv = genElement(output, 'div', '');
+  const que1 = genElement(mainDiv, 'div', question.question);
+  const optsDiv = genElement(output, 'div', '');
+  answers.forEach(opt => {
+    const opt1 = genElement(optsDiv, 'button', opt);
+    opt1.addEventListener('click', (e) => {
+      if (opt == question.correct_answer){
+        console.log('correct');
+      }else {
+        console.log('wrong');
+      }
+    })
+  });
 /*   game.que.forEach(element => {
     console.log(element);
   }); */
 }
 
-function genElement (parent, eleType, html) {
+function genElement(parent, eleType, html) {
   const temp = document.createElement(eleType);
   temp.innerHTML = html;
   parent.append(temp);
