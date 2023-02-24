92bcfc30a5ec539a5b8cc350c7b69c99daf8eda7
claudioferreira <claudioferreirajr@hotmail.com>
2023-02-24 01:56:41 -0300

    first commit trivial api

---
+++ 11. TriviaDBgame/app.js
@@ -0,0 +1,12 @@
+const btn1 = document.querySelector('.btn');
+const h1 = document.querySelector('.h1');
+const output = document.querySelector('.output');
+const inputVal = document.querySelector('.val');
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  console.log('DOM ready'); // msg sempre que o conteudo do DOM é carregado
+})
+
+btn1.addEventListener('click', (e) => {
+  console.log('ready');
+})
