49bd279c70b23b5fc828346adc0f940d405ac121
claudioferreira <claudioferreirajr@hotmail.com>
2023-02-24 02:21:30 -0300

    base url

--- 11. TriviaDBgame/app.js
+++ 11. TriviaDBgame/app.js
@@ -2,11 +2,17 @@ const btn1 = document.querySelector('.btn');
 const h1 = document.querySelector('.h1');
 const output = document.querySelector('.output');
 const inputVal = document.querySelector('.val');
+const baseURL = 'https://opentdb.com/api.php?';
 
 window.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM ready'); // msg sempre que o conteudo do DOM é carregado
+  btn1.textContent = 'Start Game';
+  inputVal.setAttribute('type', 'number');
+  inputVal.value = 10;
 })
 
 btn1.addEventListener('click', (e) => {
   console.log('ready');
+  let tempURL = baseURL + 'amount=' + inputVal.value;
+  console.log(tempURL);
 })
